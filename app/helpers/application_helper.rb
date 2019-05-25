ec0939cdbf2ea58d6a1ced8b9dc6c105c142c777
Julio Bertty <julio.bertty@gmail.com>
2019-05-25 16:48:47 -0500

    added image of gravatar on navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,11 @@ module ApplicationHelper
         markdown.render(text).html_safe
     end
 
+    def gravatar_url(email, size)
+      gravatar_id = Digest::MD5::hexdigest(email).downcase  
+      default_url = "http://mysite.com/myavatar.png" # Acá puedo poner una ruta para una imagen propia.  
+      default_url = "retro"  
+      url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI::escape(default_url)}"  
+    end
+
 end
